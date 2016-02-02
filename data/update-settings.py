8fe025a7900f0501fc15b31b71d5ed27a5d337ca
Graham Gilbert <graham@grahamgilbert.com>
2016-02-02 16:25:23 +0000

    Handle not setting a cert

--- data/update-settings.py
+++ data/update-settings.py
@@ -10,12 +10,24 @@ data['PUPPETDB_PORT']       = os.getenv('PUPPETDB_PORT', 8081)
 try:
     if os.getenv('PUPPETDB_SSL_VERIFY', True).lower() == 'true':
         data['PUPPETDB_SSL_VERIFY'] = True
+    elif:
+        os.getenv('PUPPETDB_SSL_VERIFY', False).lower() == 'false':
+            data['PUPPETDB_SSL_VERIFY'] = False
     else:
-        data['PUPPETDB_SSL_VERIFY'] = False
+        data['PUPPETDB_SSL_VERIFY'] = os.getenv('PUPPETDB_SSL_VERIFY', False)
 except:
-    DEBUG = False
-data['PUPPETDB_KEY']        = os.getenv('PUPPETDB_KEY', None)
-data['PUPPETDB_CERT']       = os.getenv('PUPPETDB_CERT', None)
+    data['PUPPETDB_SSL_VERIFY'] = True
+
+# Have we specified a key and a cert?
+try:
+    data['PUPPETDB_KEY'] = os.getenv('PUPPETDB_KEY')
+except:
+    pass
+
+try:
+    data['PUPPETDB_CERT'] = os.getenv('PUPPETDB_CERT')
+except:
+    pass
 data['PUPPETDB_TIMEOUT']    = os.getenv('PUPPETDB_TIMEOUT', 20)
 data['DEV_LISTEN_HOST']     = os.getenv('DEV_LISTEN_HOST', '127.0.0.1')
 data['DEV_LISTEN_PORT']     = os.getenv('DEV_LISTEN_PORT', 5000)
